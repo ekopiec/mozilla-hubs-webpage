48b8006b2efa6ad699dda65bccbe9074b5a6b713
liz.kopiec <ea.kopiec@gmail.com>
2021-06-16 03:57:19 +0000

    fixing s3 bucket policy

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,28 +1,26 @@
 resource "aws_s3_bucket" "webpage" {
   bucket = var.webpage_domain
   acl = "public-read"
-  # policy = aws_s3_bucket_policy.pub_ro
-  
   website {
     index_document = var.webpage_index
   }
 }
 
-#resource "aws_s3_bucket_policy" "pub_ro" {
-#  bucket = aws_s3_bucket.webpage.id
-#  policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Sid       = "PublicReadGetObject"
-#        Effect    = "Allow"
-#        Principal = "*"
-#        Action    = "s3:GetObject"
-#        Resource = [
-#          aws_s3_bucket.site.arn,
-#          "${aws_s3_bucket.webpage.arn}/*",
-#        ]
-#      },
-#    ]
-#  })
-#}
+resource "aws_s3_bucket_policy" "pub_ro" {
+  bucket = aws_s3_bucket.webpage.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.webpage.arn,
+          "${aws_s3_bucket.webpage.arn}/*",
+        ]
+      },
+    ]
+  })
+}
