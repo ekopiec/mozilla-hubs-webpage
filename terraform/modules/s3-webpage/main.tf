97711b59602447ee9cffac7a3ea7a650d9044842
liz.kopiec <ea.kopiec@gmail.com>
2021-06-17 03:01:39 +0000

    adding cert creation and cert validation dns records

--- terraform/modules/s3-webpage/main.tf
+++ terraform/modules/s3-webpage/main.tf
@@ -14,10 +14,10 @@ resource "aws_s3_bucket_policy" "pub_ro" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
         Principal = "*"
-        Action    = "s3:GetObject"
+        Action = "s3:GetObject"
         Resource = [
           aws_s3_bucket.webpage_bucket.arn,
           "${aws_s3_bucket.webpage_bucket.arn}/*",
@@ -43,8 +43,32 @@ resource "aws_route53_zone" "main" {
 #Create CNAME record
 resource "aws_route53_record" "dev-ns" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "${var.subdomain}.${var.domain}"
-  type    = "CNAME"
-  ttl     = "60"
+  name = "${var.subdomain}.${var.domain}"
+  type = "CNAME"
+  ttl = "60"
   records = [aws_s3_bucket.webpage_bucket.bucket_domain_name]
+}
+
+#Create SSL cert
+resource "aws_acm_certificate" "ssl_cert" {
+  domain_name = "${var.subdomain}.${var.domain}"
+  validation_method = "DNS"
+}
+
+#Create DNS validation records
+resource "aws_route53_record" "dns_cert_validation" {
+  for_each = {
+    for opt in aws_acm_certificate.ssl_cert.domain_validation_options : opt.domain_name => {
+      name = opt.resource_record_name
+      record = opt.resource_record_value
+      type = opt.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = aws_route53_zone.main.zone_id
 }
